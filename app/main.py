3af541c2c9b21755fdc68ad93827488392ec158f
Rebekka Haley <rebekka.haley@gmail.com>
2022-11-24 18:02:27 +0100

    [feat/predict] added prediction post

--- app/main.py
+++ app/main.py
@@ -3,18 +3,14 @@
 
 import base64
 import pickle
+from io import BytesIO
 from pathlib import Path
 
 import numpy as np
-from cv2 import (COLOR_BGR2GRAY, IMREAD_COLOR, INTER_LINEAR, cvtColor,
-                 imdecode, resize)
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-# Load prebuilt model
-model_filename = Path.cwd() / 'model.pkl'
-model = pickle.load(open(model_filename, 'rb'))
+from PIL import Image
 
 # Initialize app
 app = FastAPI()
@@ -23,6 +19,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+# Load prebuilt model
+model_filename = Path.cwd() / 'model.pkl'
+model = pickle.load(open(model_filename, 'rb'))
+
 # Handle GET request
 @app.get("/")
 async def home(request: Request):
@@ -30,39 +30,21 @@ async def home(request: Request):
 
 # Handle POST request
 @app.post("/")
-def canvas(request: Request):
-
+async def canvas(canvasimg=Form()):
     # Recieve base64 data from the user form
-    canvasdata = request.form['canvasimg']
-    encoded_data = request.form['canvasimg'].split(',')[1]
+    image_data = canvasimg.split(',')[1]
 
     # Decode base64 image to python array
-    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-    img = imdecode(nparr, IMREAD_COLOR)
+    image_data = base64.b64decode(image_data)
+    image_data = BytesIO(image_data)
 
-    # Convert 3 channel image (RGB) to 1 channel image (GRAY)
-    gray_image = cvtColor(img, COLOR_BGR2GRAY)
+    # Convert image from RGB to grayscale
+    image = Image.open(image_data).convert("L").resize((28, 28))
 
-    # Resize to (28, 28)
-    gray_image = resize(gray_image, (28, 28), interpolation=INTER_LINEAR)
+    # Turn into an array of numbers
+    image2arr = np.array(image)
+    reshaped_image = image2arr.reshape(1, 28*28)
 
-    # Expand to numpy array dimenstion to (1, 28, 28)
-    img = np.expand_dims(gray_image, axis=0)
-
-    try:
-        prediction = np.argmax(model.predict(img))
-        print(f"Prediction Result : {str(prediction)}")
-        return templates.TemplateResponse(
-            "drawing.html",
-            context={
-                "request": request,
-                "response": str(prediction),
-                "canvasdata": canvasdata,
-                "success": True})
-    except Exception as e:
-        return templates.TemplateResponse(
-            "drawing.html",
-            context={
-                "request": request,
-                "response": str(e),
-                "canvasdata": canvasdata})
+    # Run prediction
+    prediction = np.argmax(model.predict(reshaped_image))
+    return {"Prediction": f"{prediction}"}
