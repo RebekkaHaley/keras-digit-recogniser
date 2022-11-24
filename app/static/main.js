1f65f89cad0c2d319ee033d3cded36f30016e5c5
Rebekka Haley <rebekka.haley@gmail.com>
2022-11-24 13:41:07 +0100

    [feat] add canvas logic

--- app/static/main.js
+++ app/static/main.js
@@ -1,11 +1,37 @@
 const canvas = document.getElementById("canvas")
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
+canvas.height = 280
+canvas.width = 280
 
-// ctx is the context of our canvas
-// we use ctx to draw on the canvas
 const ctx = canvas.getContext("2d")
 
-// lets create a rectangle for testing purposes
-ctx.fillStyle = "red"
-ctx.fillRect(100, 100, 100, 100)
+let prevX = null
+let prevY = null
+
+ctx.lineWidth = 5
+
+let draw = false
+
+// Set draw to true when mouse is pressed
+window.addEventListener("mousedown", (e) => draw = true)
+// Set draw to false when mouse is released
+window.addEventListener("mouseup", (e) => draw = false)
+
+window.addEventListener("mousemove", (e) => {
+    // if draw is false then we won't draw
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    prevX = currentX
+    prevY = currentY
+})
