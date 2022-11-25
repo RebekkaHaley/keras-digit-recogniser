45c6287aaecd96cc0fb9f44d31ce7d84ec00d286
Rebekka Haley <rebekka.haley@gmail.com>
2022-11-25 11:42:16 +0100

    [refactor] remove nav to fix scale issue

--- app/static/main.js
+++ app/static/main.js
@@ -45,7 +45,7 @@ window.addEventListener("mousemove", (e) => {
     // If draw is true then draw
     context.fillStyle = "white"
     context.strokeStyle = "white"
-    context.lineWidth = 15
+    context.lineWidth = 30
     context.lineCap = "round"
 
     let currentX = e.clientX
