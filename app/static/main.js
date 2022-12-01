7ad9f3e8bffedbaed5eb3c914bf7542838475bd8
Rebekka Haley <rebekka.haley@gmail.com>
2022-12-01 17:38:51 +0100

    [refactor/js] fix cursor draw on canvas

--- app/static/main.js
+++ app/static/main.js
@@ -1,13 +1,47 @@
 const canvas = document.getElementById("canvas")
+canvas.style.position = "relative"
 
 const context = canvas.getContext("2d")
-// Start with black background
-context.fillStyle = "black"
-context.fillRect(0, 0, canvas.width, canvas.height)
 
 let prevX = null
 let prevY = null
 let draw = false
+
+// Start with black background
+context.fillStyle = "black"
+context.fillRect(0, 0, canvas.width, canvas.height)
+
+// Sets draw to true when mouse is pressed
+window.addEventListener("mousedown", (e) => draw = true)
+
+// Sets draw to false when mouse is released
+window.addEventListener("mouseup", (e) => draw = false)
+
+// Draws on canvas
+window.addEventListener("mousemove", (e) => {
+    // If draw is false then we won't draw
+    if (prevX == null || prevY == null || !draw) {
+        prevX = e.layerX
+        prevY = e.layerY
+        return
+    }
+    // If draw is true then draw
+    context.fillStyle = "white"
+    context.strokeStyle = "white"
+    context.lineWidth = 25
+    context.lineCap = "round"
+
+    let currentX = e.layerX
+    let currentY = e.layerY
+
+    context.beginPath()
+    context.moveTo(prevX, prevY)
+    context.lineTo(currentX, currentY)
+    context.stroke()
+
+    prevX = currentX
+    prevY = currentY
+});
 
 // Cleans entire canvas
 let clearBtn = document.querySelector("#clear")
@@ -29,33 +63,3 @@ const canvastoimage = () => {
     const canvas = document.querySelector("#canvas");
     document.getElementById("uri").value = canvas.toDataURL();
 };
-
-// Set draw to true when mouse is pressed
-window.addEventListener("mousedown", (e) => draw = true)
-// Set draw to false when mouse is released
-window.addEventListener("mouseup", (e) => draw = false)
-
-window.addEventListener("mousemove", (e) => {
-    // If draw is false then we won't draw
-    if (prevX == null || prevY == null || !draw) {
-        prevX = e.clientX
-        prevY = e.clientY
-        return
-    }
-    // If draw is true then draw
-    context.fillStyle = "white"
-    context.strokeStyle = "white"
-    context.lineWidth = 25
-    context.lineCap = "round"
-
-    let currentX = e.clientX
-    let currentY = e.clientY
-
-    context.beginPath()
-    context.moveTo(prevX, prevY)
-    context.lineTo(currentX, currentY)
-    context.stroke()
-
-    prevX = currentX
-    prevY = currentY
-});
